d19e331bee03f46e70e8b377fc5ff9592041e82a
NathanG <nathangoddard@me.com>
2015-05-31 23:36:21 +0100

    a

--- bin/restart_nginx.sh
+++ bin/restart_nginx.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# Run on server with sudo.
 # Update virtual host file, and restart nginx
 
-sudo sed -i "s/^        server .*/        server $1;/" /etc/nginx/sites-available/test
+sudo sed -i "s/^        server .*/        server $1;/" /etc/nginx/sites-available/blog.nathansplace.co.uk
 sudo service nginx restart
 
