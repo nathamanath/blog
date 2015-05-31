6f0175cea9c3ea12a3a355793e9d0d33fdcf2d86
NathanG <nathangoddard@me.com>
2015-05-31 22:01:12 +0100

    a

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bundle exec rake assets:precompile
-bundle exec puma -b unix:///app/current/tmp/sockets/puma.sock
+bundle exec puma -b unix:///app/tmp/sockets/puma.sock
 
 /usr/sbin/nginx
 
